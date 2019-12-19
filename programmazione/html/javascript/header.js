024794387229b358728795ece496538b05bd43db
Michele Torroni <michele.torroni@studio.unibo.it>
2019-12-19 20:13:21 +0100

    javascript orange animation

--- programmazione/html/javascript/header.js
+++ programmazione/html/javascript/header.js
@@ -1,27 +1,74 @@
 var tendinaAttuale;
+var paninoAttuale;
+var lastId;
+var lastX;
+var lastY;
 
-function onHoverCategorie()
-{
-    var item = document.getElementById("Tendina");
-    tendinaAttuale = item.getAttribute('src');
-    item.setAttribute('src', './img/icone/TendinaArancione.png');
-}
-
-function offHoverCategorie()
-{
-    var item = document.getElementById("Tendina");
-    item.setAttribute('src', tendinaAttuale);
-}
 
 function clickMenuAPanino(){
-        var x = document.getElementById("header");
-        var y = document.getElementById("MenuAPanino");
+  var x = document.getElementById("header");
+  var y = document.getElementById("MenuAPanino");
+  if (x.className === "Header") {
+    x.className += "Open";
+    paninoAttuale = y.getAttribute('src');
+    y.setAttribute('src', './img/icone/MenuAPaninoArancione.png');
+  } else {
+    x.className = "Header";
+    y.setAttribute('src', paninoAttuale);
+  }
+}
 
-        if (x.className === "Header") {
-          x.className += "Open";
-          y.setAttribute('src', './img/icone/MenuAPaninoArancione.png');
-        } else {
-          x.className = "Header";
-          y.setAttribute('src', './img/icone/MenuAPanino.png');
-        }
+function onHoverCategorie(){
+  var item = document.getElementById("Tendina");
+  tendinaAttuale = item.getAttribute('src');
+  item.setAttribute('src', './img/icone/TendinaArancione.png');
+}
+
+function offHoverCategorie(){
+  var item = document.getElementById("Tendina");
+  item.setAttribute('src', tendinaAttuale);
+}
+
+
+
+function onHoverGenerico() {
+  var x = event.clientX;
+  var y = event.clientY;
+  var elementMouseIsOver = document.elementFromPoint(x, y);
+  var id = elementMouseIsOver.getAttribute('id');
+  if(id != null && id.includes("li")){
+    lastX = x;
+    lastY = y;
+    lastId = id;
+    id = id.replace("li", "")
+    if(id.includes("Alto")){
+      id = id.replace("Alto", "");
+    }
+    var logo = document.getElementById(id);
+    var srcLogo = logo.getAttribute('src');
+    if(!srcLogo.includes("Arancione")){
+      srcLogo = srcLogo.replace(".png", "Arancione.png");
+    }
+    logo.setAttribute('src', srcLogo);
+  }
+}
+  
+function offHoverGenerico() {
+  X = event.clientX;
+  Y = event.clientY;
+  var elementMouseIsOver = document.elementFromPoint(X, Y);
+  if(elementMouseIsOver.tagName != "H1" && elementMouseIsOver.tagName != "IMG" && elementMouseIsOver.tagName != "A" ){
+    var elementMouseIsOver = document.elementFromPoint(lastX, lastY);
+    var id = elementMouseIsOver.getAttribute('id');
+    if(id != null && id.includes("li")){
+      id = id.replace("li", "")
+      if(id.includes("Alto")){
+        id = id.replace("Alto", "");
+      }
+      var logo = document.getElementById(id);
+      var srcLogo = logo.getAttribute('src');
+      srcLogo = srcLogo.replace("Arancione.png", ".png");
+      logo.setAttribute('src', srcLogo);
+    }
+  } 
 }
---
+++ programmazione/html/javascript/utility.js
@@ -0,0 +1,25 @@
+var lastArrowX;
+var lastArrowY;
+
+function onHoverFreccia() {
+    lastArrowX = event.clientX;
+    lastArrowY = event.clientY;
+    var elementMouseIsOver = document.elementFromPoint(lastArrowX, lastArrowY);
+    var id = elementMouseIsOver.getAttribute('id');
+    if(id.includes("Sx")){
+        elementMouseIsOver.setAttribute('src', './img/icone/FrecciaSxArancione.png');
+    } else if (id.includes("Dx")){
+        elementMouseIsOver.setAttribute('src', './img/icone/FrecciaDxArancione.png');
+    }
+}
+  
+function offHoverFreccia() {
+    var Logo = document.elementFromPoint(lastArrowX, lastArrowY);
+    var id = Logo.getAttribute('id');
+    if(id.includes("Sx")){
+        Logo.setAttribute('src', './img/icone/FrecciaSx.png');
+    } else if (id.includes("Dx")){
+        Logo.setAttribute('src', './img/icone/FrecciaDx.png');
+    }
+}
+  

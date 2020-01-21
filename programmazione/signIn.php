c3914c8c3c2e2ddf0b0cd3c3b4545148d191bd79
Andrea Tassani <andrea.tassani@studio.unibo.it>
2020-01-21 10:11:16 +0100

    very small modify

--- programmazione/signIn.php
+++ programmazione/signIn.php
@@ -4,14 +4,16 @@ require_once("dbh.php");
 $templateParams["csss"] = $linkingCss->linkingCss("signIn");
     $templateParams["contenuto"]="contenutoSignIn.php";
     $templateParams["nomePagina"] = "SIGN IN";
-
+//controllo se parametri sono specificati
 if(isset($_POST["Nome"]) && isset($_POST["Cognome"]) && 
    isset($_POST["NickName"]) && isset($_POST["Email"]) &&
    isset($_POST["Password"]) && isset($_POST["RePassword"])&&
    isset($_POST["TipoAccount"])){
-    if($_POST["Password"] == ($_POST["RePassword"])) {
+     //controllo se le due password inserite sono diverse, mando errore, altrimenti continuo
+    if($_POST["Password"] != ($_POST["RePassword"])) {
         $templateParams["erroreSignIn"] = "Errore! Le due password inserite non corrispondono";
     } else {
+      //controllo se nel db è già registrato utente con stessa email e nickname
         $login_result = $dbh->checkAlreadyExist($_POST["NickName"], $_POST["Email"]);
         if(count($login_result)== 0){
         //nessuna corrispondenza trovata, procedere con registrazione
